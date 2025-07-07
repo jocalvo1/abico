fffca880e10d0df24b5e225a6164be96625cf671
Joseph Emmanuel <josephemmanuel.xb@gmail.com>
2025-07-07 13:00:47 -0700

    Edit 1.4

---
+++ sales/history.php
@@ -0,0 +1,19 @@
+        <?php
+          include __DIR__ . "/../templates/links.php";
+        ?>
+      <!-- Sidebar -->
+        <?php 
+          include __DIR__ . "/../templates/sidebar.php";
+        ?>
+
+      <div class="main-panel">
+        
+        <?php
+          include __DIR__ . "/../templates/header.php";
+        ?>
+
+
+
+        <?php 
+          include __DIR__ . "/../templates/footer.php";
+        ?>
